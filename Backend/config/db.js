7adf3c1ccff63bc71ff23abbe3c06a3ebc9ac66d
Dileep-git09 <dileepguglavath9@gmail.com>
2026-03-20 10:53:04 +0530

    fix sum errors

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -19,23 +19,3 @@ module.exports = connectDB;
 
 
 
-
-// const dns = require("dns").promises;
-
-// const mongoose = require("mongoose");
-
-
-// dns.setServers(["1.1.1.1", "1.0.0.1"]);
-
-// const connectDB = async () => {
-//     try {
-//         const conn = await mongoose.connect(process.env.MONGO_URI);
-
-//         console.log(`MongoDB Connected: ${conn.connection.host}`);
-//     } catch (error) {
-//         console.error("MongoDB connection error:", error.message);
-//         process.exit(1);
-//     }
-// };
-
-// module.exports = connectDB;
